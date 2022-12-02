bc749f5742f9f6af267b7c0199edf7bbd0423be7
techbrv <116866892+techbrv@users.noreply.github.com>
2022-12-03 01:40:02 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://murtuzaalisurti.github.io"
+    site: "https://techbrv.github.io"
 });
